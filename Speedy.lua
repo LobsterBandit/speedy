6fd228f1b60d7999705d77267c0ab4b3d7edfb0e
LobsterBandit <code.lobsterbandit@outlook.com>
2021-07-19 21:13:45 -0400

    Fix display of encoded string when exporting (#7)

--- Speedy.lua
+++ Speedy.lua
@@ -47,6 +47,68 @@ local SpeedyDB_defaults = {
 }
 
 ------------------------------------
+-- base64 encode/decode functions
+------------------------------------
+
+local base64 = {}
+-- character table string
+base64.b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+function base64:encode(data)
+    return ((data:gsub(
+        ".",
+        function(x)
+            local r, b = "", x:byte()
+            for i = 8, 1, -1 do
+                r = r .. (b % 2 ^ i - b % 2 ^ (i - 1) > 0 and "1" or "0")
+            end
+            return r
+        end
+    ) .. "0000"):gsub(
+        "%d%d%d?%d?%d?%d?",
+        function(x)
+            if (#x < 6) then
+                return ""
+            end
+            local c = 0
+            for i = 1, 6 do
+                c = c + (x:sub(i, i) == "1" and 2 ^ (6 - i) or 0)
+            end
+            return self.b:sub(c + 1, c + 1)
+        end
+    ) .. ({"", "==", "="})[#data % 3 + 1])
+end
+
+function base64:decode(data)
+    data = gsub(data, "[^" .. self.b .. "=]", "")
+    return (data:gsub(
+        ".",
+        function(x)
+            if (x == "=") then
+                return ""
+            end
+            local r, f = "", (self.b:find(x) - 1)
+            for i = 6, 1, -1 do
+                r = r .. (f % 2 ^ i - f % 2 ^ (i - 1) > 0 and "1" or "0")
+            end
+            return r
+        end
+    ):gsub(
+        "%d%d%d?%d?%d?%d?%d?%d?",
+        function(x)
+            if (#x ~= 8) then
+                return ""
+            end
+            local c = 0
+            for i = 1, 8 do
+                c = c + (x:sub(i, i) == "1" and 2 ^ (8 - i) or 0)
+            end
+            return string.char(c)
+        end
+    ))
+end
+
+------------------------------------
 -- Event Handlers
 ------------------------------------
 
@@ -185,7 +247,7 @@ function Speedy:ShowExportString()
     local data = json.encode(self.db.global.Characters)
 
     local compressed = LibDeflate:CompressZlib(data)
-    local printable = LibDeflate:EncodeForPrint(compressed)
+    local printable = base64:encode(compressed)
 
     local frame = AceGUI:Create("Frame")
     frame:SetTitle("Speedy Character Export")
